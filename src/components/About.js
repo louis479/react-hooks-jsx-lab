a811cc68ada59283db807b01066e31d323738317
louis-ngugi <louisngugi858@gmail.com>
2025-01-19 16:53:17 +0300

    updated About.js Home.js NavBar.js and data.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/user";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+  <h2>About Me</h2>
+  <p>This is a sample paragraph about me.</p>
+  <img src={image} alt="I made this" />
+</div>;
 }
 
 export default About;
